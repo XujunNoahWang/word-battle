2d4d744bd22f251fc9992634bc1bf43621441b43
Xujun-Wang <xujun.wang@edu.sait.ca>
2025-07-01 17:47:53 +0800

    v0.3.8.1: 优化Railway部署配置

--- server.js
+++ server.js
@@ -800,10 +800,7 @@ io.on('connection', (socket) => {
 
 // 启动服务器
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`服务器运行在端口 ${PORT}`);
   console.log('\n🎮 Word Battle 已启动！');
-  // console.log(`📍 本地访问: http://localhost:${PORT}`);
-  // console.log('🌐 局域网访问: http://[你的IP]:${PORT}');
-  // console.log('服务器已准备好接受连接...\n');
 });
